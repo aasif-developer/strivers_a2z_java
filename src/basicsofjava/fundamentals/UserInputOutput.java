045db82796fa3a4318839fe971767bc2219c8b30
aasif-create <aasifmmd12345@gmail.com>
2026-01-26 14:25:20 +0530

    #1.USER INPUT AND OUTPUT , learnt how to get input and display it using java

--- src/basicsofjava/fundamentals/UserInputOutput.java
+++ src/basicsofjava/fundamentals/UserInputOutput.java
@@ -1,10 +1,26 @@
 package basicsofjava.fundamentals;
-import java.util.*;
-public class UserInputOutput {
-        public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        System.out.print("ENTER A NUMBER : ");
-        int a =obj.nextInt();
-        System.out.println("YOU HAVE ENTERED  :"+ a);
-    }
+import java.util.Scanner;
+public class UserInputOutput
+{
+        public static void main(String[] args)
+        {
+                System.out.println("1.INTEGER AS AN INPUT");
+                Scanner obj = new Scanner(System.in);
+                System.out.print("ENTER A NUMBER : ");
+                int a =obj.nextInt();
+                System.out.println("YOU HAVE ENTERED  :"+ a);
+                System.out.println("2.CHARACTER AS AN INPUT");
+                System.out.print("ENTER A CHARACTER : ");
+                char b=obj.next().charAt(0);
+                System.out.println("YOU HAVE ENTERED  :"+ b);
+                System.out.println("3.STRING AS AN INPUT");
+                System.out.print("ENTER A STRING : ");
+                obj.nextLine();
+                String c=obj.nextLine();
+                System.out.println("YOU HAVE ENTERED  :"+ c);
+                System.out.println("4.FLOAT AS AN INPUT");
+                System.out.print("ENTER A DECIMAL NUMBER : ");
+                float d=obj.nextFloat();
+                System.out.println("YOU HAVE ENTERED  :"+ d);
+        }
 }
