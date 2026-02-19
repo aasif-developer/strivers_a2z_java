b14612f7a6e8c62efef5854641d00fd189e6bf8b
aasif-create <aasifmmd12345@gmail.com>
2026-02-19 20:18:52 +0530

    PRACTISED BASIC FOR LOOP PROGRAMS

--- src/basicsofjava/fundamentals/WhileLoop.java
+++ src/basicsofjava/fundamentals/WhileLoop.java
@@ -21,6 +21,22 @@ public class WhileLoop {
             i++;
         }
         while(i<=N);
+        System.out.println("SUM OF NUMBERS FROM 1 TO N");
+        System.out.println("ENTER N VALUE");
+        int N1=sc.nextInt();
+        int sum=0;
+        for(int i7=1;i7<=N1;i7++)
+        {
+          sum=sum+i7;
+        }
+        System.out.println("SUM OF NUMBERS 1 TO "+N1+" IS " +sum);
+        //O(1) COMPLEXITY
+        int N2=5;
+        int sum1 =N2*(N2+1)/2;
+        System.out.println(sum1);
+
+
+
     }
 }
 
