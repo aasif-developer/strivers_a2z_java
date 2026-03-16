c19cbb1f9321590712b3cfe651a183bab17a6e35
aasif-create <aasifmmd12345@gmail.com>
2026-03-16 19:58:49 +0530

    PRACTICED STRING METHODS

--- src/basicsofjava/fundamentals/Strings.java
+++ src/basicsofjava/fundamentals/Strings.java
@@ -24,6 +24,17 @@ public class Strings{
         System.out.println("a.equals(b) : " + a.equals(b));
         System.out.println("a.equals(c) : " + a.equals(c));
 
-        System.out.println("");
+        System.out.println("STRING METHODS");
+        System.out.println("STRINGS ARE IMMUTABLE \n KEY REASONS:\n 1.SECURITY \n 2.STRING CONSTANT POOL EFFICIENCY \n 3.THREADS USABILITY \n" +
+                " 4.HASH CODE EFFICIENCY \n 5.CLASS LOADABILITY ");
+        System.out.println("CONCATENATION");
+        String s="JAVA ";
+        String S=new String("DEVELOPER");
+        System.out.println(s.concat(S));
+
+        System.out.println("EXPLANTION : \n concat() method creates a new String and make it as JAVA DEVELOPER \n" +
+                "whereas s remain JAVA and S remains DEVELOPER its not changed as Strings are immutable");
+
+
     }
 }
